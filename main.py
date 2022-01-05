79490a10edb23b22633ec78bdf9195c9aef129f3
doggysir <78118570+doggysir@users.noreply.github.com>
2022-01-05 18:49:48 -0500

    bugfixes

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import random
 import time
-url = open('url.txt', 'r').read
-
-webhook = DiscordWebhook(url=url, content='<@&908912623188336700>')
-
+theurl = open('url.txt', 'r').read
+webhook = DiscordWebhook(url=theurl, content='e')
+lines = open('randoms.txt', 'r').readlines()
 
 while True:
+    
     linenum = random.randint(1,22)
-    lines = open('random.txt', 'r').readlines
-    linecount = 0
-    for x in lines:
-        linecount += 1
+
+    for linecount, x in enumerate(lines, start=1):
         if linecount == linenum:
             line = x
 
