2cbbb1d53fd5ed2735329e73fe05ed292c6239f3
doggysir <78118570+doggysir@users.noreply.github.com>
2022-01-06 22:56:54 -0500

    it works

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from nextcord.ext import commands
 import aiohttp
 from nextcord.webhook.async_ import AsyncWebhookAdapter
 import asyncio
-theurl = open('url.txt', 'r').read
-client = commands.Bot(command_prefix='-')
-lines = open('randoms.txt', 'r').readlines()
+theurl = open('url.txt', 'r').read()
+
+client = commands.Bot(command_prefix='-jjkwdhak')
+lines = open('randoms.txt', 'r').readlines
 
 async def sendToWebhook(content):
-    async with aiohttp.ClientSession as session:
+    async with aiohttp.ClientSession() as session:
         webhook = nextcord.Webhook.from_url(theurl, session=session)
         await webhook.send(content)
 
@@ -20,11 +21,14 @@ async def sendToWebhook(content):
 async def on_ready():
     while True:
         num = random.randint(1, 22)
-        for linecount, x in enumerate(lines(), start=1):
+        linecount = 0
+        for x in lines():
+            linecount += 1
             if linecount == num:
                 line = x
-        sendToWebhook( f'Hello <@&908912623188336700> we would revive the chat with a topic.The topic is : {line}')
+        await sendToWebhook( f'Hello <@&908912623188336700> we would revive the chat with a topic.The topic is : {line}')
         await asyncio.sleep(3600)
+
 
 
 tokenyes = open('token.txt', 'r').read()
