bfadbbe3466e72b52cbd02bd396579ec1a5cccd3
doggysir <78118570+doggysir@users.noreply.github.com>
2022-01-06 22:41:10 -0500

    changed to nextcord

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-from discord_webhook import DiscordWebhook, DiscordEmbed
+from nextcord import Webhook
+import nextcord
 import random
 import time
+from nextcord.ext import commands
+import aiohttp
+from nextcord.webhook.async_ import AsyncWebhookAdapter
+import asyncio
 theurl = open('url.txt', 'r').read
-webhook = DiscordWebhook(url=theurl, content='e')
+client = commands.Bot(command_prefix='-')
 lines = open('randoms.txt', 'r').readlines()
 
-while True:
-    
-    linenum = random.randint(1,22)
+async def sendToWebhook(content):
+    async with aiohttp.ClientSession as session:
+        webhook = nextcord.Webhook.from_url(theurl, session=session)
+        await webhook.send(content)
 
-    for linecount, x in enumerate(lines, start=1):
-        if linecount == linenum:
-            line = x
 
-    embed = DiscordEmbed(title="Dead Chat Revive", description=line, color='03b2f8')
-    webhook.add_embed(embed)
-    webhook.execute()
-    time.sleep(3600)
+@client.event
+async def on_ready():
+    while True:
+        num = random.randint(1, 22)
+        for linecount, x in enumerate(lines(), start=1):
+            if linecount == num:
+                line = x
+        sendToWebhook( f'Hello <@&908912623188336700> we would revive the chat with a topic.The topic is : {line}')
+        await asyncio.sleep(3600)
 
+
+tokenyes = open('token.txt', 'r').read()
+client.run(tokenyes)
